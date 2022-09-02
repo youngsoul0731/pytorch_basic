f8d1b97a5d1bd26ef584a4884c40f9a18e3cfa72
youngsoul0731 <21009100594@stu.xidian.edu.cn>
2022-09-02 20:56:19 +0800

    mnist_work
    读取mnist数据集，并展示部分图片以及对应的标签

---
+++ mnist_read/minst_work.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+from torch import nn
+from torchvision.datasets import mnist
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+#训练集，测试集合批次划分
+train_batch_size = 64
+test_batch_size = 128
+#优化器参数设定
+learning_rate=0.01
+lr=0.01
+momentum=0.5
+#训练循环总次数
+num_epoches=20
+# 对数据集进行划分
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5],[0.5])])
+train_dataset = mnist.MNIST('./data', train=True, transform=transform)
+test_dataset = mnist.MNIST('./data', train=False, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=test_batch_size, shuffle=False)
+
+# 打印图像数据
+examples=enumerate(test_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+fig=plt.figure()
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.tight_layout()
+    plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+    plt.title("Ground Truth: {}".format(example_targets[i]))
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
+
+
